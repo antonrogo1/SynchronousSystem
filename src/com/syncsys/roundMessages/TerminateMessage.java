f55b5bbdb501a31a044bf5a4b98f18a999673f55
Daren Cheng <2012DCheng@gmail.com>
2017-02-26 18:25:30 -0600

    Adding missing terminate message file

---
+++ src/com/syncsys/roundMessages/TerminateMessage.java
@@ -0,0 +1,30 @@
+package com.syncsys.roundMessages;
+
+import com.syncsys.ProcessNode;
+import com.syncsys.roundStrategies.BellmanFordStrategy;
+import com.syncsys.roundStrategies.RoundStrategy;
+
+public class TerminateMessage implements RoundMessage {
+	private int senderID;
+	
+	public TerminateMessage() {
+		setSenderID(-1);
+	}
+
+	@Override
+    public void processUsing(RoundStrategy strategy) {
+		BellmanFordStrategy bfStrategy = (BellmanFordStrategy)strategy;
+		ProcessNode process = bfStrategy.getProcess();
+		
+		process.setTerminating(true);
+    }
+	
+	public int getSenderID() {
+	    return senderID;
+    }
+
+	public void setSenderID(int senderID) {
+	    this.senderID = senderID;
+    }
+
+}
